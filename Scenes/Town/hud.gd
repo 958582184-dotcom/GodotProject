225f9aefd871159c045bb072ac31959a29556dba
Jor <958582184@qq.com>
2026-09-16 17:02:34 +0800

    UI界面

---
+++ Scenes/Town/hud.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+class_name  HUD
+
+
+
+func _on_inventory_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_equipment_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_statts_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_skill_button_pressed() -> void:
+	pass # Replace with function body.
